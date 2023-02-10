90d4e76481d28957bc3c5b6a347954e85f51588e
Thiago Bezerra de Carvalho <e107ti@cps.sp.gov.br>
2023-02-10 19:41:41 -0300

    tentei desenrolar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+//parte do "robô"
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Imagine que você está discutindo. O que você diria para vencer a discussão?");
+Console.ResetColor();
+
+//parte do usuário
+Console.ForegroundColor = ConsoleColor.Magenta;
+string semberrar = Console.ReadLine()!;
+string BERRANDO = semberrar.ToUpper();  
+Console.ResetColor();
+
+//resultado
+Console.WriteLine("\nE VOCÊ É UM CABEÇUDO!!!");
+Console.ReadKey();
+
+Console.WriteLine($"\n{BERRANDO}!!!!");
