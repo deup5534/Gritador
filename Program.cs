2ea5117a2d06afbb5b65758abe140ef21ade10df
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2023-03-01 19:25:55 -0300

    sofisticado

--- Program.cs
+++ Program.cs
@@ -1,18 +1,40 @@
 ﻿Console.Clear();
 
-//parte do "robô"
+//apresentação
+
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Imagine que você está discutindo. O que você diria para vencer a discussão?");
 Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("\npressione uma tecla para continuar...");
+Console.ResetColor();
+Console.ReadKey();
+
+Console.Clear();
+
 //parte do usuário
+
+Console.Write("Escreva:");
 Console.ForegroundColor = ConsoleColor.Magenta;
 string semberrar = Console.ReadLine()!;
 string BERRANDO = semberrar.ToUpper();  
 Console.ResetColor();
 
-//resultado
-Console.WriteLine("\nE VOCÊ É UM CABEÇUDO!!!");
+Console.Clear();
+
+//"robô"
+
+Console.Write("Adversário:");
+Console.WriteLine("E VOCÊ É UM PALERMA!!!");
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("\npressione uma tecla para continuar...");
+Console.ResetColor();
 Console.ReadKey();
 
+
+//usuário
+
+Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine($"\n{BERRANDO}!!!!");
+Console.ResetColor();
